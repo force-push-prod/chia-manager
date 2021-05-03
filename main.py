66ec4a54cf25edcb3a211713472d14790e216ba4
Yifei Yin <yifeiyin@foxmail.com>
2021-05-02 22:25:55 -0400

    Most functionalities implemented

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ import datetime
 
 from timeago import relative_format
 
-testlog = open('testlog.log', 'r')
+testlog = open('/dev/stdin', 'r')
 testlog = [line.strip().split() for line in testlog.readlines()]
+
+
+TAB = '\t'
+NL = '\n'
+
+def parse_iso_datetime(s):
+    return datetime.datetime.strptime(s, '%Y-%m-%dT%H:%M:%S')
+
+
+class NextPlotException(Exception): pass
 
 def rfc_date_to_datetime(s):
     # [parsing - How to parse a RFC 2822 date/time into a Python datetime? - Stack Overflow](https://stackoverflow.com/questions/885015)
@@ -20,50 +30,72 @@ def format_seconds(s: float | int):
     s = int(s)
     return f'({s // 3600}.{s % 3600 // 60}.{s % 60 // 1}) {s}s'
 
-@dataclass
+
+
 class PlotInfo:
-    plot_id = ''
-    plot_size = 0
-    plot_total_count = 0
-    start_time = datetime.datetime(2000, 1, 1)
-    config_buffer_size = ''
-    config_buckets = 0
-    config_threads = 0
-    config_threads_stripe_size = 0
-    dir_tmp1 = ''
-    dir_tmp2 = ''
-    # dir_dst = ''
+    def __init__(self):
+        self.plot_id: str = ''
+        self.plot_size = 0
+        self.plot_total_count = 0
+        self.start_time = datetime.datetime(2000, 1, 1)
+        self.config_buffer_size = ''
+        self.config_buckets = 0
+        self.config_threads = 0
+        self.config_threads_stripe_size = 0
+        self.dir_tmp1 = ''
+        self.dir_tmp2 = ''
+        # self.dir_dst = ''
 
     def __str__(self):
         return pprint.pformat(self.__dict__)
 
-# @dataclass
-# class PlotState:
-#     stage = 0
-#     current_bucket = 0
-#     error = ''
+    @property
+    def summary(self):
+        return (
+            '     id: ' + str(self.plot_id) + '\n'
+            ' buffer: ' + str(self.config_buffer_size.replace('MiB', ' MiB')) + '\n'
+            'threads: ' + str(self.config_threads) + '\n'
+            '\n'
+            'dir_tmp: ' + self.dir_tmp1 + '  ' + self.dir_tmp1 + '\n'
+        )
 
-@dataclass(init=True)
+
+
+
+MAX_TABLE = 7
+
 class PlotProgress:
-    stages_start_time = {}
-    stages_took_seconds = {}
-    total_time_seconds = 0
-    current_bucket = 0
-
-    def __str__(self):
-        return pprint.pformat({
-            'total_time_seconds': self.total_time_seconds,
-            'current_bucket': self.current_bucket,
-            'stages_start_time': { k: format_time(v) for k, v in self.stages_start_time.items()},
-        })
+    def __init__(self):
+        self.stages_start_time = {}
+        self.stages_took_seconds = {}
+        self.total_time_seconds = 0.0
+        self.current_bucket = 0
+        self.current_table = 0
+        self.error = ''
 
     @property
     def current_stage(self):
         """Returns current stage. 0 is not started; 5 is finished
         """
-        if self.total_time_seconds:
+        if self.total_time_seconds != 0.0:
             return 5
         return max([0, *self.stages_start_time.keys()])
+
+    @property
+    def current_stage_progress(self):
+        # if self.current_stage == 0 or self.current_stage == 5:
+        #     return 'N/A'
+        def o(a, b, c, d):
+            string = f'{a} / {b} of {c} / {d}'.replace(' of 1 / 1', '')
+            ratio = c / d + ((1 / d) * (a / b))
+            return string, ratio
+
+        match self.current_stage:
+            case 1: return o(self.current_bucket, 128, self.current_table, MAX_TABLE)
+            case 2: return o(self.current_bucket, 2, 7 - self.current_table, MAX_TABLE - 1)
+            case 3: return o(self.current_bucket, 110, self.current_table / 2, MAX_TABLE)
+            case 4: return o(self.current_bucket, 128, 0, 1)
+            case _: return 'N/A', 1
 
     @property
     def summary(self):
@@ -72,38 +104,82 @@ class PlotProgress:
         elif self.current_stage == 5:
             status = 'finished'
         else:
-            status = 'stage ' + self.current_stage
+            status = 'stage ' + str(self.current_stage)
 
         stages_strings = []
-        for stage_n in range(1, 5):
+        for stage_n in range(1, self.current_stage):
             start_time = self.stages_start_time.get(stage_n, '')
             if start_time: start_time = format_time(start_time)
 
             took = self.stages_took_seconds.get(stage_n, '')
             if took: took = format_seconds(took)
 
-            stages_strings.append('Stage ' + str(stage_n) + '   ' + str(took) + '\t  ' + str(start_time))
+            stages_strings.append(f'Stage {stage_n} {TAB} {start_time} {TAB} {took}')
         stages_strings = '\n'.join(stages_strings)
 
-        total_time = format_seconds(self.total_time_seconds) if self.total_time_seconds else '-'
+        current_stage_progress = ''
+
+        if self.current_stage != 0 and self.current_stage != 5:
+            seconds_elapsed = (datetime.datetime.now() - self.stages_start_time[1]).total_seconds()
+
+            progress_string, progress_ratio = self.current_stage_progress
+
+            eta_seconds = (seconds_elapsed / (progress_ratio or 0.01))
+            eta_time = self.stages_start_time[self.current_stage] + datetime.timedelta(seconds=eta_seconds)
+            current_stage_progress = f'''
+stage {self.current_stage}:
+    progress  {progress_string}                   {int(progress_ratio * 100)}%
+    elapsed   {format_seconds(seconds_elapsed)}
+    ETA       {format_seconds(eta_seconds)}        on {relative_format(eta_time)}
+'''.strip()
+
+        total_time = ''
+        if self.total_time_seconds:
+            total_time = 'total time: ' + format_seconds(self.total_time_seconds)
 
         return f"""
 status: {status}
-
 {stages_strings}
 
-total time: {total_time}
-        """
+{current_stage_progress}
+
+{total_time}
+
+""".strip().replace('\n\n\n', '\n\n')
+
 
 @dataclass
 class Plot:
-    info = PlotInfo()
-    progress = PlotProgress()
+    def __init__(self):
+        self.info = PlotInfo()
+        self.progress = PlotProgress()
+        self.error = ''
+        self.last_alive = ''
 
     def __str__(self):
-        return str(self.info) + '\n' + str(self.progress)
+        return self.summary
+
+    @property
+    def summary(self):
+        s = (
+            '-' * 80 + '\n'
+            '' + self.info.summary + '\n'
+            '' + self.progress.summary + '\n'
+        )
+
+        if self.error != '':
+            s += ('*' * 80 + '\n') * 3 + self.error
+
+        if self.last_alive != '':
+            s += '\nlast alive:    ' + relative_format(self.last_alive) + '\n'
+
+        return s
+
 
     def consume_line(self, line):
+        if len(line) > 0 and line[0].startswith('2021-') and 'chia.plotting' not in ''.join(line):
+            self.last_alive = parse_iso_datetime(line.pop(0))
+
         match line:
             case [timestamp, 'chia.plotting.create_plots', ':', 'INFO', 'Creating', plot_total_count, 'plots', *_]:
                 self.info.start_time = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%f')
@@ -111,14 +187,16 @@ class Plot:
 
             # Starting plotting progress into temporary dirs
             case ['Starting', 'plotting', *_, 'dirs:', dir_tmp1, 'and', dir_tmp2]:
+                if self.progress.current_stage != 0:
+                    raise NextPlotException()
                 self.info.dir_tmp1 = dir_tmp1
                 self.info.dir_tmp2 = dir_tmp2
 
             case ['ID:', x]: self.info.plot_id = x
-            case ['Plot', 'size', 'is:', x]: self.info.plot_size = int(x)
-            case ['Buffer', 'size', 'is:', x]: self.info.config_buffer_size = x
-            case ['Using', x, 'buckets']: self.info.config_buckets = int(x)
-            case ['Using', x, 'threads', _, 'size', y]:
+            case ['Plot', 'size', 'is:', x]:    self.info.plot_size = int(x)
+            case ['Buffer', 'size', 'is:', x]:  self.info.config_buffer_size = x
+            case ['Using', x, 'buckets']:       self.info.config_buckets = int(x)
+            case ['Using', x, 'threads', *_, 'size', y]:
                 self.info.config_threads = int(x)
                 self.info.config_threads_stripe_size = int(y)
 
@@ -127,25 +205,54 @@ class Plot:
 
                 phase_number = phase[0]
                 assert phase_number in '1234'
-                # breakpoint()
                 self.progress.stages_start_time[int(phase_number)] = start_time
-
-            case ['Bucket', x, 'uniform', 'sort.', *_]:
-                self.progress.current_bucket = int(x)
 
             case ['Total', 'time', '=', x, 'seconds.', 'CPU', *_]:
                 self.progress.total_time_seconds = float(x)
 
-            case ['Time', 'for', 'phase', phase_number, '=', seconds, *_, t1, t2, t3, t4, t5]:
-                # finish_time = rfc_date_to_datetime(' '.join(t1, t2, t3, t4, t5))
+            case ['Time', 'for', 'phase', phase_number, '=', seconds, *_]:
                 self.progress.stages_took_seconds[int(phase_number)] = float(seconds)
 
-            case [_]:
-                pass
+            # Phrase 1
+            case ['Computing', 'table', x]:
+                self.progress.current_table = int(x) - 1
 
-p = Plot()
-for line in testlog:
-    p.consume_line(line)
+            # Phrase 2
+            case ['Backpropagating', 'on', 'table', x]:
+                self.progress.current_table = MAX_TABLE - (int(x) - 1)
+                self.progress.current_bucket = 0
 
-# print(p)
-print(p.progress.summary)
+            # Phrase 2
+            case ['scanned', 'table', *_]:
+                self.progress.current_bucket = 1
+
+            # Phrase 3
+            case ['Compressing tables', x, 'and', _]:
+                self.progress.current_table = (int(x) - 1) * 2
+
+            # Phrase 3
+            case ['First', 'computation', 'pass', 'time', *_]:
+                self.progress.current_table += 1
+
+            # Phrase 1, 3, 4
+            case ['Bucket', x, 'uniform', 'sort.', *_]:
+                self.progress.current_bucket = int(x)
+
+            case [*x]:
+                s = ' '.join(x)
+                if 'err' in s.lower():
+                    self.error = s
+
+
+plots = [Plot()]
+for i, line in enumerate(testlog):
+    try:
+        plots[-1].consume_line(line)
+    except NextPlotException:
+        plots.append(Plot())
+        print(plots)
+        plots[-1].consume_line(line)
+
+
+for p in plots:
+    print(p.summary)
