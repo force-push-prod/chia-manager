d12ac524eb5fdd2d02542fa5732e30b967d28fb0
Dennis Mo <dennismoyunfei@gmail.com>
2021-05-04 22:47:59 -0700

    new bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -8,7 +8,7 @@ class BootStrap:
 
     def start_plot(self, command=None, log_file=None):
         with open(sys.argv[1], 'wb') as out:
-            self.plot_process = subprocess.Popen(sys.argv[2:], stdout=out, stderr=out)
+            self.plot_process = subprocess.Popen(' '.join(sys.argv[2:]) + ' | ts %Y-%m-%dT%H:%M:%S%z', stdout=out, stderr=out, shell=True)
 
     def print_process_info(self):
         print(self.plot_process.pid)
