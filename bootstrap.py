51c998b92610ad01bee858e589b48847b5600e8b
Yifei Yin <yifeiyin@foxmail.com>
2021-05-05 01:54:30 -0400

    Update bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -1,11 +1,16 @@
 import subprocess
 import sys
 
-    def start_plot(self, command=None, log_file=None):
-        with open(sys.argv[1], 'wb') as out:
-            self.plot_process = subprocess.Popen(' '.join(sys.argv[2:]) + ' | ts %Y-%m-%dT%H:%M:%S%z', stdout=out, stderr=out, shell=True)
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Too few arguments.')
+        exit(1)
 
     with open(sys.argv[1], 'wb') as out:
-        plot_process = subprocess.Popen(sys.argv[2:], stdout=out, stderr=out)
-
+        plot_process = subprocess.Popen(
+            ' '.join(sys.argv[2:]) + ' | ts %Y-%m-%dT%H:%M:%S%z',
+            stdout=out,
+            stderr=out,
+            shell=True
+        )
     print(plot_process.pid)
